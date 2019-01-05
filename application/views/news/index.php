dcfc43838c30d3f42d516a5bf03aabe60bad78cd
Alexandre Caldeira <zuacaldeira@gmail.com>
2019-01-05 21:15:39 +0100

    Add separator between articles in news page.

--- application/views/news/index.php
+++ application/views/news/index.php
@@ -6,19 +6,21 @@
         <?php echo $title; ?>
         <small class="text-muted">(<?php echo count($news); ?> articles)</small>
         <?php if($username != null): ?>
-            <a class="btn btn-sm btn-primary d-inline float-right" href="news/create">Write Article</a>
+        <a class="btn btn-sm btn-primary d-inline float-right" href="news/create">Write Article</a>
         <?php endif; ?>
     </h2>
 
 
     <?php foreach($news as $news_item): ?>
-    <h3>
-        <?php echo $news_item['title']; ?>
-    </h3>
-    <div class="main">
-        <?php echo $news_item['text'];  ?>
+    <div class="border-bottom py-3">
+        <h3>
+            <?php echo $news_item['title']; ?>
+        </h3>
+        <div class="main">
+            <?php echo $news_item['text'];  ?>
+        </div>
+        <p><a href="<?php echo site_url('news/'.$news_item['slug']); ?>">View Article</a></p>
     </div>
-    <p><a href="<?php echo site_url('news/'.$news_item['slug']); ?>">View Article</a></p>
 
     <?php endforeach; ?>
 </div>

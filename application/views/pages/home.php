32c8498728396f72dbaa5c9d1c142010765ac943
Alexandre Caldeira <zuacaldeira@gmail.com>
2019-01-08 16:38:29 +0100

    Center home content with jquery-ui.

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,12 +1,19 @@
-<div class="align-bottom">
-<h2 class="mt-5 mb-3">Welcome to Nsemi</h2>
-<div>
-    <p class="text-muted display-3">Image Transformation Tool for Image Lovers
-    </p>
+<div class="home-content">
+    <h2 class="mt-5 mb-3">Welcome to Nsemi</h2>
     <div>
-        <a class="btn btn-sm btn-outline-secondary my-1 text-left" href="<?php echo base_url(); ?>tools">Resize images, Create thumbnails, Convert images, Monetize your Images</a>
-        <a class="btn btn-sm btn-outline-secondary my-1 text-left" href="<?php echo base_url(); ?>gallery">Search our image gallery</a>
-        <a class="btn btn-sm btn-outline-secondary my-1 text-left" href="<?php echo base_url(); ?>news">Development Blog and News</a>
+        <p class="text-muted display-3">Image Transformation Tool for Image Lovers
+        </p>
+        <div>
+            <a class="btn btn-sm btn-outline-secondary my-1 text-left" href="<?php echo base_url(); ?>tools">Resize images, Create thumbnails, Convert images, Monetize your Images</a>
+            <a class="btn btn-sm btn-outline-secondary my-1 text-left" href="<?php echo base_url(); ?>gallery">Search our image gallery</a>
+            <a class="btn btn-sm btn-outline-secondary my-1 text-left" href="<?php echo base_url(); ?>news">Development Blog and News</a>
+        </div>
     </div>
 </div>
-</div>
+<script>
+$(document).ready(function() {
+    $('.home-content').position({
+        of: 'body main'
+    });
+});
+</script>

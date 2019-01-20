97a9210d9111c44bcbddb961c31b25438b84c01d
Alexandre Caldeira <zuacaldeira@gmail.com>
2019-01-20 19:30:33 +0100

    Add article tag to image upload form.

--- application/views/gallery/create.php
+++ application/views/gallery/create.php
@@ -1,16 +1,29 @@
-<h2 class="my-5">Nsemi Image Processing Tool</h2>
+<article class="container">
+    <h2 class="my-5">Nsemi Image Processing Tool</h2>
+
+    <?php echo form_open_multipart('gallery/create'); ?>
+
+    <div class="clearfix my-1">
+        <input id="data" type="file" name="data" class="" />
+        <button type="submit" id="btn-upload" value="Upload" class="btn btn-sm btn-primary float-right" title="Upload Image" disabled>Upload</button>
+    </div>
+
+    <div id="previewer" class="border border-secondary my-2 w-100 overflow-auto position-relative" style="width: 100%;">
+        <img src="" />
+    </div>
 
 
-<?php echo form_open_multipart('gallery/create'); ?>
+    </form>
 
-<div class="clearfix my-1">
-    <input id="data" type="file" name="data" class=""/>
-    <button type="submit" id="btn-upload" value="Upload" class="btn btn-sm btn-primary float-right" title="Upload Image" disabled>Upload</button>
-</div>
-
-<div id="previewer" class="border border-secondary my-2 w-100 overflow-auto position-relative" style="width: 100%;">
-    <img src="" />
-</div>
+</article>
 
 
-</form>
+<script src="<?php echo base_url(); ?>assets/js/upload.js">
+
+
+</script>
+
+<script src="<?php echo base_url(); ?>assets/js/previewer.js">
+
+
+</script>

b518802714bb9c6ee6a8530d1bc10b942103c5b2
Alexandre Caldeira <zuacaldeira@gmail.com>
2019-02-07 16:43:59 +0100

    Remove CI user guide.

--- application/controllers/Logout.php
+++ application/controllers/Logout.php
@@ -1,4 +1,4 @@
-<?php
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Logout extends CI_Controller {
     public function __construct() {
--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -1,4 +1,4 @@
-<?php
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Migrate extends CI_Controller
 {
